7c0496748fa065a09bae101b649e912b91fbb518
Vitor Miyazaki <vitormiya@hotmail.com>
2025-08-03 18:59:48 -0300

    feat(page): Altera estrutura do page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,32 +4,34 @@ import Card from '@/components/Card';
 export default function Dashboard() {
   return (
       <>
-          <Header />
-          <main className="max-w-[1200px] mx-auto">
-            <div className="w-full grid grid-cols-1 gap-[24px] sm:gap-[32px] 2xl:grid 2xl:grid-cols-3 2xl:gap-[24px]">
-              oi
-            <Card width={'w-[180px]'} height={'h-auto'}>
-              <span>
-                e ai
-              </span>
-            </Card>
-            <div className="w-full">
-              <Card background={'bg-gray-300'}>
+        <Header/>
+        <main className="w-full flex items-center justify-center h-full absolute">
+          <div className="w-full max-w-[1440px] grid grid-cols-[1fr_3fr_2fr] h-full gap-4">
+            <Card >OLA POVO</Card>
+            <div className="flex flex-col w-full h-full">
+              <Card background={ 'bg-gray-300' }>
+                <div className="">
+                  e ai
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                </div>
+              </Card>
+              <Card background={ 'bg-cyan-900' }>
                 <div className="">
                   e ai
                 </div>
               </Card>
-              <Card background={'bg-cyan-900'}>
-            <div className="">
-              e ai
             </div>
-          </Card>
-            </div>
-            <Card>
-              <div className=""></div>
-            </Card>
-            </div>
-          </main>
+            <Card height={'h-[512px]'}>Erro encontrado!</Card>
+          </div>
+        </main>
       </>
   )
 }
