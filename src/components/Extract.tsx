fc0c431010c7edc92d2656cea9016f4ddf2a1e86
Vitor Miyazaki <vitormiya@hotmail.com>
2025-08-10 18:26:29 -0300

    feat(Extract): Adiciona nova altura ao extrato

--- src/components/Extract.tsx
+++ src/components/Extract.tsx
@@ -65,7 +65,7 @@ export function Extract() {
           </div>
         </div>
 
-        <div>
+        <div className={'max-h-[800px] overflow-auto'}>
           {transactions === null || transactions.length === 0 ? (
               <div className="text-gray-500 text-center py-8">
                 Nenhuma transação encontrada
