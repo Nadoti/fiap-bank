ad0a5ac61408300d412fb2a3ba9b4ba5805c2f72
Douglas Nadoti <dodo_nadoti@hotmail.com>
2025-08-10 15:13:54 -0300

    implementar novo component, cricao de transacoes

--- src/components/features/dashboard/DashboardViewer.tsx
+++ src/components/features/dashboard/DashboardViewer.tsx
@@ -2,20 +2,76 @@
 
 import Header from '@/components/features/header/Header';
 import Balance from '@/components/Balance';
-import { Card, Select } from 'miragem-ds';
+import { Button, Card, CurrencyInput, Input, Select } from 'miragem-ds';
 import CurrentDate from '@/components/CurrentDate';
 import { Extract } from '@/components/Extract';
 import { Menu } from '@/components/Menu';
 import img from "../../../../public/images/img-woman-credit-card-md@2x.png"
+import React from 'react';
+import { useLocalStorage } from '@/hooks/useStorage';
+import { TransactionType } from '@/enum/transaction.enum';
 
+
+
+export interface SelectIOption {
+  label: string;
+  value: string;
+}
+
+const sampleOptions: SelectIOption[] = [
+  { value: "currency-exchange", label: "Câmbio de Moeda" },
+  { value: "doc-ted", label: "DOC/TED" },
+  { value: "loan-financing", label: "Empréstimo e Financiamento" },
+  { value: "investments", label: "Investimentos" },
+];
+
+interface Transaction {
+  id: string
+  type: string;
+  value: number;
+  createdAt: string;
+}
 
 export function DashboardViewer() {
+  const [typeTransaction, setTypeTransaction] = React.useState('')
+  const [valueTransaction, setValueTransaction] = React.useState(0)
+  const { getStorage, setStorage} = useLocalStorage();
+
+  function createTransaction() {
+    console.log(valueTransaction)
+    if(typeTransaction && valueTransaction) {
+      const existingTransactions = getStorage<Transaction[]>(TransactionType.TRANSACTION) || [];
+      console.log('existingTransactions', existingTransactions)
+      const newTransaction: Transaction = {
+        id: crypto.randomUUID(),
+        type: typeTransaction,
+        value: valueTransaction,
+        createdAt: new Date().toISOString()
+      };
+      console.log('newTransaction', newTransaction)
+      const updatedTransactions = [...existingTransactions, newTransaction];
+      console.log('updatedTransactions', updatedTransactions)
+      setStorage(TransactionType.TRANSACTION, updatedTransactions);
+      
+      setTypeTransaction('');
+      setValueTransaction(0);
+    }
+  }
+  
+  const handleValorChange = (value: number | '') => {
+    if (value === '') {
+      setValueTransaction(0);
+    } else {
+      setValueTransaction(value);
+    }
+  };
+
   
   return (
     <>
       <Header/>
-      <main className="w-full flex items-center justify-center h-full absolute mt-8">
-        <div className="w-full max-w-[1440px] grid grid-cols-[1fr_3fr_2fr] h-full gap-4 ">
+      <main className="w-full flex items-center justify-center h-full mt-8">
+        <div className="w-full max-w-[1440px] grid grid-cols-[1fr_4fr_2fr] h-full gap-4 ">
 
           <Menu />
 
@@ -50,11 +106,34 @@ export function DashboardViewer() {
               isPixelsImages={true} 
               sidePixelsImages='right'
             >
-              <div className="">
-                <h3>Nova transação</h3>
-                {/* <Select>
+              <div className="p-2">
+                <h3 className='text-2xl font-bold text-gray-100 mb-8'>Nova transação</h3>
+                <div className='w-full max-w-96 mb-8'>
+                  <Select
+                    options={sampleOptions}
+                    value={typeTransaction}
+                    onChange={(type) => setTypeTransaction(type)}
+                    placeholder='Selecione o tipo de transação'
+                    label=''
+                    fullWidth={false}
+                  />
+                </div>
+                <div className='w-full max-w-72 mb-8'>
+                  {/* <Input label='Valor' variant='default' onChange={(value) => setValueTransaction(value)}/> */}
+                  <CurrencyInput
+                    label="Valor"
+                    value={valueTransaction}
+                    onChange={handleValorChange}
+                    placeholder="0,00"
+                  />
+      
                   
-                </Select> */}
+                </div>
+                <div className='w-full max-w-72 mb-8'>
+                  <Button fullWidth type='button' onClick={createTransaction}>
+                    Concluir transação
+                  </Button>
+                </div>
               </div>
             </Card>
 
---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,42 @@
+import { useCallback } from 'react';
+
+export function useLocalStorage() {
+  const getStorage = useCallback(<T>(key: string): T | null => {
+    if (typeof window === 'undefined') return null;
+   
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    } catch (error) {
+      console.error('Erro ao ler localStorage:', error);
+      return null;
+    }
+  }, []);
+
+  const setStorage = useCallback(<T>(key: string, value: T): void => {
+    if (typeof window === 'undefined') return;
+   
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error('Erro ao salvar no localStorage:', error);
+    }
+  }, []);
+
+
+  const removeStorage = useCallback((key: string) => {
+    if (typeof window === 'undefined') return;
+   
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error('Erro ao remover do localStorage:', error);
+    }
+  }, []);
+
+  return {
+    getStorage,
+    setStorage,
+    removeStorage,
+  };
+}
