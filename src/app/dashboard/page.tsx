6e44a16fe01dc6ad4d06fdb170ff3cbf625f9dbb
Douglas Nadoti <dodo_nadoti@hotmail.com>
2025-08-10 20:51:00 -0300

    mudanca pagina onde fica os components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
-import { DashboardViewer } from "@/components/features/dashboard/DashboardViewer";
-import { TransactionsProvider } from "@/contexts/TransactionsContext";
+
 
 
 export default function Dashboard() {
   
 
   return (
-    <TransactionsProvider>
-      <DashboardViewer />
-    </TransactionsProvider>
+    <p>aa</p>
   )
 }
